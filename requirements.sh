3a52ba6024b25bf1fe7f8f21b1d55ad7772d850a
overlamer <65023172+overlamer@users.noreply.github.com>
2020-06-17 21:27:01 +0300

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,11 @@
+#!/data/data/com.termux/files/usr/bin/
+rm -rf ~/../usr/bin/autoalias
+cp autoalias ~/../usr/bin/
+cd ~/../usr/bin/
+chmod +x autoalias
+cd $HOME
+clear
+echo " "
+echo -e '\e[34m                   Введи\e[1;31m autoalias \e[34mдля запуска!\e[0m'
+cd
+rm -rf AutoAlias
